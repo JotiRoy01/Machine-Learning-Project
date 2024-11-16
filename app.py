629a9d0ca5cd22dbfb15035e050ec4add0d27393
JotiRoy01 <jotiroygit@gmail.com>
2024-11-16 16:33:54 +0600

    add log erro

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import Housing_Exception
+import sys
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET", "POST"])
 
 def index() :
+    try : 
+        raise Exception("We are testing custom exception")
+    except Exception as e :
+        
+        housing = Housing_Exception(e, sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
     return "Starting Machine Learning Project"
 
 if __name__ == "__main__" :
